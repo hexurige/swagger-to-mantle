ca38f6812a286560bd3e9cfe7fb00659e1cbec68
Xu He <xuhe@assetfinda.com>
2017-07-28 15:12:55 +1200

    V0.1

--- swagger2mtl.py
+++ swagger2mtl.py
@@ -1,22 +1,130 @@
-import re
+# ====
+# Swagger Class
+# Created by xhe585, on 28/Jul/2017.
+# ====
+import json
+import os
+import time
 
+class Swagger(object):
+    """Swagger"""
 
-class Column(object):
-    def __init__(self, name, type_name, optional):
-        self.name = name
-        self.typeName = type_name
-        self.optional = optional
-        self.fieldName = self.get_field_name()
+    def __init__(self, j):
+        self.__dict__ = json.loads(j)
+        self.host = self.host
+        self.models = []
+        for definition in self.definitions:
+            self.models.append(Model(self.definitions[definition]))
 
     def __str__(self):
-        rtr = 'Column Name: ' + self.name + '\n'
-        rtr += 'Type Name: ' + self.typeName + '\n'
-        rtr += 'Optional: ' + self.optional + '\n'
+        rtr = ''
+        for m in self.models:
+            rtr += m.__str__()
+
+        return rtr
+
+    def create_mantles_directory(self):
+        if not os.path.exists('mantles'):
+            os.makedirs('mantles')
+
+    def create_mantles(self):
+        self.create_mantles_directory()
+
+        for model in self.models:
+            model.create_header_file()
+            model.create_implement_file()
+
+
+class Model(object):
+    """Model"""
+
+    def __init__(self, dict):
+        self.__dict__ = dict
+        self._properties = []
+        self.file_name = 'AFM{0}'.format(self.title)
+        self.mantle_path = 'mantles'
+        for p in self.properties:
+            self._properties.append(Property(self.properties[p]))
+
+    def __str__(self):
+        rtr = 'Model Title: {0} | Type: {1} | Number of Properties: {2}\n'.format(self.title, self.type, len(self._properties))
+        return rtr
+
+    def get_comment(self, is_header):
+        if not os.path.exists('comment'):
+            return ''
+        else:
+            rtr = '//\n'
+            if is_header:
+                rtr += '//  {0}.h\n'.format(self.file_name)
+            else:
+                rtr += '//  {0}.m\n'.format(self.file_name)
+
+            with open('comment') as f:
+                lines = f.readlines()
+                for line in lines:
+                    rtr += line
+            
+            rtr += '//  On {0}\n'.format(time.strftime("%d/%m/%Y"))
+            rtr += '//\n'
+            return rtr
+
+    def create_header_file(self):
+        file_name = '{0}.h'.format(self.file_name)
+        f = open(os.path.join(self.mantle_path, file_name), 'w')
+        f.write(self.get_header_file_content())
+        f.close()
+
+    def create_implement_file(self):
+        file_name = '{0}.m'.format(self.file_name)
+        f = open(os.path.join(self.mantle_path, file_name), 'w')
+        f.write(self.get_implement_file_content())
+        f.close()
+
+
+    def get_JSONKeyPathsByPropertyKey_method(self):
+        rtr = ''
+        rtr += '+(NSDictionary*)JSONKeyPathsByPropertyKey{\n'
+        rtr += '\treturn @{\n'
+        for p in self._properties:
+            rtr += '\t\t@"{0}" : @"{1}",\n'.format(p.title, p.get_field_name())
+                
+        rtr += '\t};\n'
+        rtr += '}\n\n'
+        return rtr
+
+    def get_header_file_content(self):
+        rtr = self.get_comment(True)
+        rtr += '#import <Mantle/Mantle.h>\n\n'
+        rtr += '@interface {0} : MTLModel <MTLJSONSerializing>\n\n'.format(self.file_name)
+        for p in self._properties:
+            rtr += p.get_property_header()
+        rtr += '\n@end\n'
+        return rtr
+
+    def get_implement_file_content(self):
+        rtr = self.get_comment(False)
+        rtr += '#import "{0}.m\n\n'.format(self.file_name)
+        rtr += '@implementation {0}\n\n'.format(self.file_name)
+        rtr += self.get_JSONKeyPathsByPropertyKey_method()
+        for p in self._properties:
+            rtr += p.get_null_checking_funtion()
+        rtr += '\n@end\n'
+        return rtr
+
+class Property(object):
+    """Property"""
+
+    def __init__(self, dict):
+        self.__dict__ = dict
+
+    def __str__(self):
+        rtr = ''
         return rtr
 
     def get_field_name(self):
         formatted_field_name = ''
-        name_words = str.split(self.name, '_')
+        name_words = str.split(self.title, '_')
         for idx, word in enumerate(name_words):
             if idx == 0:
                 formatted_field_name = formatted_field_name + word
@@ -24,107 +132,69 @@ class Column(object):
                 formatted_field_name = formatted_field_name + word.title()
         return formatted_field_name
 
+    def get_property_header(self):
+        header = ''
+        if self.type == 'Int32':
+            header = '@property(nonatomic) int %s;\n' % self.get_field_name()
+        elif self.type == 'Decimal':
+            header = '@property(nonatomic) double %s;\n' % self.get_field_name(
+            )
+        elif self.type == 'Boolean':
+            header = '@property(nonatomic) BOOL %s;\n' % self.get_field_name()
+        elif self.type == 'String':
+            header = '@property(nonatomic, copy) NSString *%s;\n' % self.get_field_name()
+        elif self.type == 'DateTime':
+            header = '@property(nonatomic, copy) NSString *%s;\n' % self.get_field_name()
+        else:
+            pass
+        return header
 
-def delete_content(pfile):
-    pfile.seek(0)
-    pfile.truncate()
+    def get_null_checking_funtion(self):
+        # write Int32 null checking.
+        rtr = ''
 
-model_list = []
-mtl_json_f = open('mtlJson', 'w')
-mtl_header_f = open('mtlHeader', 'w')
-delete_content(mtl_json_f)
-delete_content(mtl_header_f)
+        if self.type == 'Int32':
+            rtr += '+(NSValueTransformer *)%sJSONTransformer{\n' % self.get_field_name()
+            rtr += '\treturn [MTLValueTransformer transformerWithBlock:^id(id inObj) {\n'
+            rtr += '\t\tif (inObj == nil) {\n'
+            rtr += '\t\t\treturn [NSNumber numberWithInteger:0];\n'
+            rtr += '\t\t} else {\n'
+            rtr += '\t\t\treturn inObj;\n'
+            rtr += '\t\t}\n'
+            rtr += '\t}];\n'
+            rtr += '}\n\n'
 
-with open('input') as f:
-    lines = f.readlines()
-# you may also want to remove whitespace characters like `\n` at the end of each line
-lines = [x.strip() for x in lines]
+        elif self.type == 'Decimal':
+            rtr += '+(NSValueTransformer *)%sJSONTransformer{\n' % self.get_field_name()
+            rtr += '\treturn [MTLValueTransformer transformerWithBlock:^id(id inObj) {\n'
+            rtr += '\t\tif (inObj == nil) {\n'
+            rtr += '\t\t\treturn [NSNumber numberWithDouble:0.0f];\n'
+            rtr += '\t\t} else {\n'
+            rtr += '\t\t\treturn inObj;\n'
+            rtr += '\t\t}\n'
+            rtr += '\t}];\n'
+            rtr += '}\n\n'
 
-print('### reading input..')
-print(lines)
-print('### construct models..')
+        elif self.type == 'Boolean':
+            rtr += '+(NSValueTransformer *)%sJSONTransformer{\n' % self.get_field_name()
+            rtr += '\treturn [MTLValueTransformer transformerWithBlock:^id(id inObj) {\n'
+            rtr += '\t\tif (inObj == nil) {\n'
+            rtr += '\t\t\treturn [NSNumber numberWithInteger:0];\n'
+            rtr += '\t\t} else {\n'
+            rtr += '\t\t\treturn inObj;\n'
+            rtr += '\t\t}\n'
+            rtr += '\t}];\n'
+            rtr += '}\n\n'
+
+        return rtr
 
 
-for line in lines:
-    line_properties = re.findall(r"[\w']+", line)
-    name = line_properties[0]
-    type_name = line_properties[1]
-    optional = line_properties[2]
-    model = Column(name, type_name, optional)
-    model_list.append(model)
+models = []
 
-print('### write mtl json')
+with open('swagger.json') as f:
+    content = f.read().replace('\n', '')
+    swagger = Swagger(content)
+    swagger.create_mantles()
 
-# write mapping dictionary
-mtl_json_f.write('+(NSDictionary*)JSONKeyPathsByPropertyKey{\n')
-mtl_json_f.write('\treturn @{\n')
-for model in model_list:
-    formatted_field_string = '\t\t@"%s" : @"%s",\n' % (model.fieldName, model.name)
-    mtl_json_f.write(formatted_field_string)
-mtl_json_f.write('\t};\n')
-mtl_json_f.write('}\n\n')
 
-# write Int32 null checking.
-mtl_json_f.write('#pragma Int32 check nil.\n')
-for model in model_list:
-    if model.typeName == 'Int32':
-        mtl_json_f.write('+(NSValueTransformer *)%sJSONTransformer{\n' % model.fieldName)
-        mtl_json_f.write('\treturn [MTLValueTransformer transformerWithBlock:^id(id inObj) {\n')
-        mtl_json_f.write('\t\tif (inObj == nil) {\n')
-        mtl_json_f.write('\t\t\treturn [NSNumber numberWithInteger:0];\n')
-        mtl_json_f.write('\t\t} else {\n')
-        mtl_json_f.write('\t\t\treturn inObj;\n')
-        mtl_json_f.write('\t\t}\n')
-        mtl_json_f.write('\t}];\n')
-        mtl_json_f.write('}\n\n')
-
-# write Decimal null checking.
-mtl_json_f.write('#pragma Decimal check nil.\n')
-for model in model_list:
-    if model.typeName == 'Decimal':
-        mtl_json_f.write('+(NSValueTransformer *)%sJSONTransformer{\n' % model.fieldName)
-        mtl_json_f.write('\treturn [MTLValueTransformer transformerWithBlock:^id(id inObj) {\n')
-        mtl_json_f.write('\t\tif (inObj == nil) {\n')
-        mtl_json_f.write('\t\t\treturn [NSNumber numberWithDouble:0.0f];\n')
-        mtl_json_f.write('\t\t} else {\n')
-        mtl_json_f.write('\t\t\treturn inObj;\n')
-        mtl_json_f.write('\t\t}\n')
-        mtl_json_f.write('\t}];\n')
-        mtl_json_f.write('}\n\n')
-
-# write Boolean null checking.
-mtl_json_f.write('#pragma Boolean check nil.\n')
-for model in model_list:
-    if model.typeName == 'Boolean':
-        mtl_json_f.write('+(NSValueTransformer *)%sJSONTransformer{\n' % model.fieldName)
-        mtl_json_f.write('\treturn [MTLValueTransformer transformerWithBlock:^id(id inObj) {\n')
-        mtl_json_f.write('\t\tif (inObj == nil) {\n')
-        mtl_json_f.write('\t\t\treturn [NSNumber numberWithInteger:0];\n')
-        mtl_json_f.write('\t\t} else {\n')
-        mtl_json_f.write('\t\t\treturn inObj;\n')
-        mtl_json_f.write('\t\t}\n')
-        mtl_json_f.write('\t}];\n')
-        mtl_json_f.write('}\n\n')
-
-mtl_json_f.close()
-
-print('### write mtl header')
-for model in model_list:
-    formatted_field_string = ''
-    if model.typeName == 'Int32':
-        formatted_field_string = '@property(nonatomic) int %s;\n' % model.fieldName
-    elif model.typeName == 'Decimal':
-        formatted_field_string = '@property(nonatomic) double %s;\n' % model.fieldName
-    elif model.typeName == 'Boolean':
-        formatted_field_string = '@property(nonatomic) BOOL %s;\n' % model.fieldName
-    elif model.typeName == 'String':
-        formatted_field_string = '@property(nonatomic, copy) NSString *%s;\n' % model.fieldName
-    elif model.typeName == 'DateTime':
-        formatted_field_string = '@property(nonatomic, copy) NSString *%s;\n' % model.fieldName
-    else:
-        print(model.typeName)
-        formatted_field_string = '#TODO *%s;\n' % model.fieldName
-
-    mtl_header_f.write(formatted_field_string)
-
-mtl_header_f.close()
+    #swagger = json.loads(content, object_hook = as_swagger)
